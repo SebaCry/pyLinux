61be17ea66de04889f126c06aa3e02bf2404cd15
sebacry3 <Jspc513@gmail.com>
2024-11-12 22:05:40 -0500

    INCREIBLEEEE DOCKERRR, solo que tengo que agarrarle el tiro, pero increibleeeeee

--- app/graphics.py
+++ app/graphics.py
@@ -24,7 +24,7 @@ def generate_pie_chart(labels, values):
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
     ax.axis('equal')
-    plt.savefig('pie2.png')
+    plt.savefig('chart_pie_finalfinal.png')
     plt.close()
 
 if __name__ == '__main__':
